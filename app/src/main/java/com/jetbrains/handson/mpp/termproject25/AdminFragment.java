b283aa58f3675f784b29fd6b4fdaa31be8b4a185
Robert Chisholm <robwchis@gmail.com>
2022-10-31 20:00:26 -0400

    work

---
+++ app/src/main/java/com/jetbrains/handson/mpp/termproject25/AdminFragment.java
@@ -0,0 +1,47 @@
+package com.jetbrains.handson.mpp.termproject25;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.jetbrains.handson.mpp.termproject25.databinding.FragmentAdminBinding;
+import com.jetbrains.handson.mpp.termproject25.databinding.FragmentFinalBinding;
+
+public class AdminFragment extends Fragment {
+
+    private FragmentAdminBinding binding;
+
+    TextView roleText, nameText;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentAdminBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+    }
+
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
--- app/src/main/java/com/jetbrains/handson/mpp/termproject25/FinalFragment.java
+++ app/src/main/java/com/jetbrains/handson/mpp/termproject25/FinalFragment.java
@@ -1,9 +1,11 @@
 package com.jetbrains.handson.mpp.termproject25;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -15,6 +17,8 @@ import com.jetbrains.handson.mpp.termproject25.databinding.FragmentFirstBinding;
 public class FinalFragment extends Fragment {
 
     private FragmentFinalBinding binding;
+
+    TextView roleText, nameText;
 
     @Override
     public View onCreateView(
@@ -30,6 +34,28 @@ public class FinalFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        Bundle bundle = this.getArguments();
+
+        if(bundle != null){
+            // handle your code here.
+            System.out.println("WORKED");
+        }
+
+
+        roleText = view.findViewById(R.id.txt_displayRole);
+        nameText = view.findViewById(R.id.txt_displayUsername);
+
+        userDBHandler userDB = new userDBHandler(FinalFragment.this.getContext());
+
+        Cursor cursor = userDB.getData();
+
+        System.out.println(cursor.getCount());
+
+        while (cursor.moveToNext()) {
+            System.out.println("OVER HERE: "+cursor.getString(0));
+            nameText.setText(cursor.getString(0));
+            break;
+        }
 
 
     }
