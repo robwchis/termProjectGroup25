a400bffd12b7951ab29644ea5d8a98f6e8d16c05
Jamie Bly <90733356+JamieBly@users.noreply.github.com>
2022-11-14 14:50:07 -0500

    Changed the Course Databases for d2

--- app/src/main/java/com/jetbrains/handson/mpp/termproject25/adminDBHandler.java
+++ app/src/main/java/com/jetbrains/handson/mpp/termproject25/adminDBHandler.java
@@ -11,7 +11,12 @@ public class adminDBHandler extends SQLiteOpenHelper {
     private static final String TABLE_NAME = "courses";
     private static final String COLUMN_COURSE_NAME ="name";
     private static final String COLUMN_COURSE_CODE = "code";
-    private static final String DATABASE_NAME = "courses.db";
+    private static final String COLUMN_COURSE_DAYONE = "day1";
+    private static final String COLUMN_COURSE_DAYTWO = "day2";
+    private static final String COLUMN_COURSE_TIMEONE = "time1";
+    private static final String COLUMN_COURSE_TIMETWO = "time2";
+    private static final String COLUMN_COURSE_DESCRIPTION = "Description";
+    private static final String DATABASE_NAME = "courseList.db";
     private static final int DATABASE_VERSION = 1;
 
 
@@ -20,7 +25,12 @@ public class adminDBHandler extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase adminDB) {
         String createTableCommand = "CREATE TABLE " + TABLE_NAME
                 + "(" + COLUMN_COURSE_NAME +" TEXT, "
-                + COLUMN_COURSE_CODE + " TEXT)";
+                + COLUMN_COURSE_CODE + " TEXT,"
+                + COLUMN_COURSE_DAYONE + " TEXT,"
+                + COLUMN_COURSE_DAYTWO + " TEXT,"
+                + COLUMN_COURSE_TIMEONE + " TEXT,"
+                + COLUMN_COURSE_TIMETWO + " TEXT,"
+                + COLUMN_COURSE_DESCRIPTION + " TEXT)";
 
         adminDB.execSQL(createTableCommand);
     }
@@ -45,6 +55,11 @@ public class adminDBHandler extends SQLiteOpenHelper {
         //Sets all the values, including the admin value, which is false by defalse* :) (*Default)
         values.put(COLUMN_COURSE_NAME, c.getName());
         values.put(COLUMN_COURSE_CODE, c.getCode());
+        values.put(COLUMN_COURSE_DAYONE, "");
+        values.put(COLUMN_COURSE_DAYTWO, "");
+        values.put(COLUMN_COURSE_TIMEONE, "");
+        values.put(COLUMN_COURSE_TIMETWO, "");
+        values.put(COLUMN_COURSE_DESCRIPTION, "");
 
         db.insert(TABLE_NAME, null, values);
         db.close();
--- app/src/main/java/com/jetbrains/handson/mpp/termproject25/course.java
+++ app/src/main/java/com/jetbrains/handson/mpp/termproject25/course.java
@@ -3,10 +3,20 @@ package com.jetbrains.handson.mpp.termproject25;
 public class course {
     private String name;
     private String code;
+    private String[] courseDays;
+    private String[] courseTimes;
+    private String courseDesc;
+    private int studentCapacity;
 
     public course(String n, String c){
         name = n;
         code = c;
+//        courseDays = new String[2];
+        courseDays[0] = "";
+        courseDays[1] = "";
+//        courseTimes = new String[2];
+        courseTimes[0] = "";
+        courseTimes[1] = "";
     }
 
     public void setName(String n){
@@ -23,6 +33,32 @@ public class course {
 
     public String getCode(){
         return code;
+    }
+
+    public void setCourseDays(String day1, String day2){
+        courseDays[0] = day1;
+        courseDays[1] = day2;
+    }
+
+    public String[] getCourseDays(){
+        return courseDays;
+    }
+
+    public void setCourseTimes(String time1, String time2){
+        courseTimes[0] = time1;
+        courseTimes[1] = time2;
+    }
+
+    public String[] getCourseTimes(){
+        return courseTimes;
+    }
+
+    public void setCourseDesc(String desc){
+        courseDesc = desc;
+    }
+
+    public String getCourseDesc(){
+        return courseDesc;
     }
 
 }
