250ffec02c6aa19dcb368caa76074a037068710d
Robert Chisholm <robwchis@gmail.com>
2022-11-16 19:53:01 -0500

    2

--- app/src/main/java/com/jetbrains/handson/mpp/termproject25/InstructorFragment2.java
+++ app/src/main/java/com/jetbrains/handson/mpp/termproject25/InstructorFragment2.java
@@ -9,7 +9,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentResultListener;
 import androidx.navigation.fragment.NavHostFragment;
@@ -31,6 +33,33 @@ public class InstructorFragment2 extends Fragment {
     String instructor;
     InstructorAdapter2 adapter;
     TextView txtUsernameIF2;
+    adminDBHandler db;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // This callback will only be called when MyFragment is at least Started.
+        OnBackPressedCallback callback = new OnBackPressedCallback(true /* enabled by default */) {
+            @Override
+            public void handleOnBackPressed() {
+                // Handle the back button event
+                System.out.println("!!!!gets here fo sure!!!!");
+                Bundle bundle = new Bundle();
+                bundle.putString("beepboop", String.valueOf(txtUsernameIF2.getText())); // Put anything what you want
+
+
+                getParentFragmentManager().setFragmentResult("beepboop",bundle);
+
+                NavHostFragment.findNavController(InstructorFragment2.this)
+                        .navigate(R.id.action_instructorFragment2_to_instructorFragment);
+
+            }
+        };
+        requireActivity().getOnBackPressedDispatcher().addCallback(this, callback);
+
+        // The callback can be enabled or disabled here or in handleOnBackPressed()
+    }
 
     @Override
     public View onCreateView(
@@ -54,7 +83,7 @@ public class InstructorFragment2 extends Fragment {
         recyclerView = view.findViewById(R.id.recyclerView2);
         TextView nameText = view.findViewById(R.id.txt_displayUsernameInst);
         txtUsernameIF2 = view.findViewById(R.id.txtUsernameIF2);
-        adminDBHandler db = new adminDBHandler(InstructorFragment2.this.getContext());
+        db = new adminDBHandler(InstructorFragment2.this.getContext());
         getParentFragmentManager().setFragmentResultListener("beepboopBundle", this, new FragmentResultListener() {
             @Override
             public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle result) {
@@ -113,19 +142,38 @@ public class InstructorFragment2 extends Fragment {
 
 
                 // DOGULASS -> this is still not working
-
-                Bundle bundle = new Bundle();
-                bundle.putString("beepboop", txtUsernameIF2.getText().toString()); // Put anything what you want
-                bundle.putBoolean("beepboop2",true);
-
-                getParentFragmentManager().setFragmentResult("beepboop",bundle);
-
-                NavHostFragment.findNavController(InstructorFragment2.this)
-                        .navigate(R.id.action_instructorFragment2_self);
-
-
+                updateStuff();
             }
         });
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
+
+    }
+
+    public void updateStuff() {
+
+        System.out.println("GETS TO THIS POINT");
+
+        adapter = new InstructorAdapter2(this.getContext(), names, codes, new InstructorAdapter2.OnButtonClickListener() {
+            @Override
+            public void onButtonClick(int pos) {
+                System.out.println("pos: "+pos);
+
+                // DOUGLASS -> right here set the instructor for the course in position 'pos' to the instructor.
+                course re = new course(tnames[pos], tcodes[pos]);
+                db.removeCourse(re);
+                re.setInstructor(txtUsernameIF2.getText().toString());
+
+                db.addCourse(re);
+
+                // here
+
+
+                // DOGULASS -> this is still not working
+                updateStuff();
+            }
+        });
+
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
 
