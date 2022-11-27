a213ba10a6b3c5737394783bab07d1adb957afe7
Jamie Bly <90733356+JamieBly@users.noreply.github.com>
2022-11-27 15:35:03 -0500

    User Stuff

--- app/src/main/java/com/jetbrains/handson/mpp/termproject25/User.java
+++ app/src/main/java/com/jetbrains/handson/mpp/termproject25/User.java
@@ -5,10 +5,41 @@ public class User {
     private int id;
     private String username;
     private String password;
+    private String course1Day1;
+    private String course1Code;
+    private String course1Day2;
+    private String course2Day1;
+    private String course2Code;
+    private String course2Day2;
+    private String course3Day1;
+    private String course3Code;
+    private String course3Day2;
+    private String course4Day1;
+    private String course4Code;
+    private String course4Day2;
+    private String course5Day1;
+    private String course5Code;
+    private String course5Day2;
 
     public User(String u, String p, int adm){
         username = u;
         password = p;
+
+        course1Code = "";
+        course1Day1 = "";
+        course1Day2 = "";
+        course2Code = "";
+        course2Day1 = "";
+        course2Day2 = "";
+        course3Code = "";
+        course3Day1 = "";
+        course3Day2 = "";
+        course4Code = "";
+        course4Day1 = "";
+        course4Day2 = "";
+        course5Code = "";
+        course5Day1 = "";
+        course5Day2 = "";
     }
 
     public void setId(int nID){
@@ -33,6 +64,82 @@ public class User {
 
     public String getPassword(){
         return password;
+    }
+
+    public void setCourse(String code, String days, String hours){
+        String[] d = days.split("/");
+        String[] h = hours.split("/");
+        if(course1Code.equals("")){
+            course1Code = code;
+            course1Day1 = d[0] +"@"+h[0];
+            course1Day2 = d[1] +"@"+h[1];
+        } else if(course2Code.equals("")){
+            course2Code = code;
+            course2Day1 = d[0] +"@"+h[0];
+            course2Day2 = d[1] +"@"+h[1];
+        } else if(course3Code.equals("")){
+            course3Code = code;
+            course3Day1 = d[0] +"@"+h[0];
+            course3Day2 = d[1] +"@"+h[1];
+        }else if(course4Code.equals("")){
+            course4Code = code;
+            course4Day1 = d[0] +"@"+h[0];
+            course4Day2 = d[1] +"@"+h[1];
+        }else if(course5Code.equals("")){
+            course5Code = code;
+            course5Day1 = d[0] +"@"+h[0];
+            course5Day2 = d[1] +"@"+h[1];
+        } else {
+            //returns an error message
+        }
+    }
+
+    public String getCourseCode(int courseNumber){
+
+        if(courseNumber == 1){
+            return course1Code;
+        } else if(courseNumber == 2){
+            return course2Code;
+        } else if(courseNumber == 3){
+            return course3Code;
+        } else if(courseNumber == 4){
+            return course4Code;
+        } else if(courseNumber == 5){
+            return course5Code;
+        }
+        return "null";
+    }
+
+    public String getCourseDay1(int courseNumber){
+
+        if(courseNumber == 1){
+            return course1Day1;
+        } else if(courseNumber == 2){
+            return course2Day1;
+        } else if(courseNumber == 3){
+            return course3Day1;
+        } else if(courseNumber == 4){
+            return course4Day1;
+        } else if(courseNumber == 5){
+            return course5Day1;
+        }
+        return "null";
+    }
+
+    public String getCourseDay2(int courseNumber){
+
+        if(courseNumber == 1){
+            return course1Day2;
+        } else if(courseNumber == 2){
+            return course2Day2;
+        } else if(courseNumber == 3){
+            return course3Day2;
+        } else if(courseNumber == 4){
+            return course4Day2;
+        } else if(courseNumber == 5){
+            return course5Day2;
+        }
+        return "null";
     }
 
 }
--- app/src/main/java/com/jetbrains/handson/mpp/termproject25/userDBHandler.java
+++ app/src/main/java/com/jetbrains/handson/mpp/termproject25/userDBHandler.java
@@ -11,8 +11,23 @@ public class userDBHandler extends SQLiteOpenHelper {
     private static final String TABLE_NAME = "users";
     private static final String COLUMN_USERNAME = "username";
     private static final String COLUMN_PASSWORD = "password";
-    private static final String COLUMN_ADMIN = "admin";
-    private static final String DATABASE_NAME = "users.db";
+    private static final String COLUMN_COURSE1_CODE = "c1code";
+    private static final String COLUMN_COURSE1_DAY1 = "c1days";
+    private static final String COLUMN_COURSE1_DAY2 = "c1hours";
+    private static final String COLUMN_COURSE2_CODE ="c2code";
+    private static final String COLUMN_COURSE2_DAY1 = "c2days";
+    private static final String COLUMN_COURSE2_DAY2 = "c2hours";
+    private static final String COLUMN_COURSE3_CODE ="c3code";
+    private static final String COLUMN_COURSE3_DAY1 = "c3days";
+    private static final String COLUMN_COURSE3_DAY2 = "c3hours";
+    private static final String COLUMN_COURSE4_CODE ="c4code";
+    private static final String COLUMN_COURSE4_DAY1 = "c4days";
+    private static final String COLUMN_COURSE4_DAY2 = "c4hours";
+    private static final String COLUMN_COURSE5_CODE ="c5code";
+    private static final String COLUMN_COURSE5_DAY1 = "c5days";
+    private static final String COLUMN_COURSE5_DAY2 = "c5hours";
+
+    private static final String DATABASE_NAME = "updatedUsers.db";
     private static final int DATABASE_VERSION = 1;
 
 
@@ -22,8 +37,23 @@ public class userDBHandler extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase userDB) {
 
         String createTableCommand = "CREATE TABLE " + TABLE_NAME
-                + "(" + COLUMN_USERNAME +" TEXT, "
-                + COLUMN_PASSWORD + " TEXT)";
+                + "(" + COLUMN_USERNAME + " TEXT, "
+                + COLUMN_PASSWORD + " TEXT, "
+                + COLUMN_COURSE1_CODE + " TEXT, "
+                + COLUMN_COURSE1_DAY1 + " TEXT, "
+                + COLUMN_COURSE1_DAY2+ " TEXT, "
+                + COLUMN_COURSE2_CODE + " TEXT, "
+                + COLUMN_COURSE2_DAY1 + " TEXT, "
+                + COLUMN_COURSE2_DAY2 + " TEXT, "
+                + COLUMN_COURSE3_CODE + " TEXT, "
+                + COLUMN_COURSE3_DAY1 + " TEXT, "
+                + COLUMN_COURSE3_DAY2 + " TEXT, "
+                + COLUMN_COURSE4_CODE + " TEXT, "
+                + COLUMN_COURSE4_DAY1 + " TEXT, "
+                + COLUMN_COURSE4_DAY2 + " TEXT, "
+                + COLUMN_COURSE5_CODE + " TEXT, "
+                + COLUMN_COURSE5_DAY1 + " TEXT, "
+                + COLUMN_COURSE5_DAY2 + " TEXT)";
 
         userDB.execSQL(createTableCommand);
 
@@ -50,6 +80,23 @@ public class userDBHandler extends SQLiteOpenHelper {
         //Sets all the values, including the admin value, which is false by defalse* :) (*Default)
         values.put(COLUMN_USERNAME, user.getUsername());
         values.put(COLUMN_PASSWORD, user.getPassword());
+        values.put(COLUMN_COURSE1_CODE, user.getCourseCode(1));
+        values.put(COLUMN_COURSE1_DAY1, user.getCourseCode(1));
+        values.put(COLUMN_COURSE1_DAY2, user.getCourseCode(1));
+        values.put(COLUMN_COURSE2_CODE, user.getCourseCode(2));
+        values.put(COLUMN_COURSE2_DAY1, user.getCourseCode(2));
+        values.put(COLUMN_COURSE2_DAY2, user.getCourseCode(2));
+        values.put(COLUMN_COURSE3_CODE, user.getCourseCode(3));
+        values.put(COLUMN_COURSE3_DAY1, user.getCourseCode(3));
+        values.put(COLUMN_COURSE3_DAY2, user.getCourseCode(3));
+        values.put(COLUMN_COURSE4_CODE, user.getCourseCode(4));
+        values.put(COLUMN_COURSE4_DAY1, user.getCourseCode(4));
+        values.put(COLUMN_COURSE4_DAY2, user.getCourseCode(4));
+        values.put(COLUMN_COURSE5_CODE, user.getCourseCode(5));
+        values.put(COLUMN_COURSE5_DAY1, user.getCourseCode(5));
+        values.put(COLUMN_COURSE5_DAY2, user.getCourseCode(5));
+
+
 
         db.insert(TABLE_NAME, null, values);
         db.close();
