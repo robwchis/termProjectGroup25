69a11c762886532f13c40d20fc50f4977d5b2fc5
Jamie Bly <90733356+JamieBly@users.noreply.github.com>
2022-11-28 15:27:14 -0500

    Update FinalFragment.java
    working on the finalfragment load button functionality

--- app/src/main/java/com/jetbrains/handson/mpp/termproject25/FinalFragment.java
+++ app/src/main/java/com/jetbrains/handson/mpp/termproject25/FinalFragment.java
@@ -110,34 +110,121 @@ public class FinalFragment extends Fragment {
                 inst = new String[5];
 
                 Cursor uc = uDB.getData();
+                Cursor c = db.getData();
+                int i = 0;
 
                 while(uc.moveToNext()){
                     if(uc.getString(0).equals(username)){
 
-                        
 
-                    }
-                }
 
-                Cursor cursor = db.getData();
+                        if (!(uc.getString(2).toString().equals(""))) {
+                            System.out.println("HAS ONE AL:READY");
 
-                int i = 0;
-                while(cursor.moveToNext()){
+                            String[] day1 = uc.getString(3).split("@");
+                            String[] day2 = uc.getString(4).split("@");
 
-                    if(cursor.getString(0).equals(username)) {
-                        System.out.println("HELLO" + username + "aa" +cursor.getString(6) );
-                        if (cursor.getString(6).equals(username)) {
-                            names[i] = cursor.getString(0);
-                            codes[i] = cursor.getString(1);
-                            days[i] = cursor.getString(2) + "/" + cursor.getString(3);
-                            hours[i] = cursor.getString(4) + "/" + cursor.getString(5);
-                            inst[i] = cursor.getString(6);
-                            descs[i] = cursor.getString(7);
-                            capacities[i] = cursor.getString(8);
+                            names[0] = uc.getString(2);
+                            days[0] = day1[0]+"/"+day2[0];
+                            hours[0] = day1[1]+"/"+day2[1];
+
+                            while(c.moveToNext()){
+                                if(c.getString(0).equals(names[0] )&& days[0].equals(c.getString(2)+"/"+c.getString(3)) && hours[0].equals(c.getString(4)+"/"+c.getString(5)) ){
+                                    codes[i] = c.getString(1);
+                                    inst[i] = c.getString(6);
+                                    descs[i] = c.getString(7);
+                                    capacities[i] = c.getString(8);
+                                }
+                            }
+
+
                             i++;
                         }
+                        if (!(uc.getString(5).toString().equals(""))) {
+
+                            String[] day1 = uc.getString(6).split("@");
+                            String[] day2 = uc.getString(7).split("@");
+
+                            names[1] = uc.getString(5);
+                            days[1] = day1[0]+"/"+day2[0];
+                            hours[1] = day1[1]+"/"+day2[1];
+
+                            while(c.moveToNext()){
+                                if(c.getString(0).equals(names[1] )&& days[1].equals(c.getString(2)+"/"+c.getString(3)) && hours[1].equals(c.getString(4)+"/"+c.getString(5)) ){
+                                    codes[i] = c.getString(1);
+                                    inst[i] = c.getString(6);
+                                    descs[i] = c.getString(7);
+                                    capacities[i] = c.getString(8);
+                                }
+                            }
+
+                            i++;
+                        }
+                        if (!(uc.getString(8).toString().equals(""))) {
+
+                            String[] day1 = uc.getString(9).split("@");
+                            String[] day2 = uc.getString(10).split("@");
+
+                            names[2] = uc.getString(8);
+                            days[2] = day1[0]+"/"+day2[0];
+                            hours[2] = day1[1]+"/"+day2[1];
+
+                            while(c.moveToNext()){
+                                if(c.getString(0).equals(names[2] )&& days[2].equals(c.getString(2)+"/"+c.getString(3)) && hours[2].equals(c.getString(4)+"/"+c.getString(5)) ){
+                                    codes[i] = c.getString(1);
+                                    inst[i] = c.getString(6);
+                                    descs[i] = c.getString(7);
+                                    capacities[i] = c.getString(8);
+                                }
+                            }
+
+                            i++;
+                        }
+                        if (!(uc.getString(11).toString().equals(""))) {
+                            String[] day1 = uc.getString(12).split("@");
+                            String[] day2 = uc.getString(13).split("@");
+
+                            names[3] = uc.getString(11);
+                            days[3] = day1[0]+"/"+day2[0];
+                            hours[3] = day1[1]+"/"+day2[1];
+
+                            while(c.moveToNext()){
+                                if(c.getString(0).equals(names[3] )&& days[3].equals(c.getString(2)+"/"+c.getString(3)) && hours[3].equals(c.getString(4)+"/"+c.getString(5)) ){
+                                    codes[i] = c.getString(1);
+                                    inst[i] = c.getString(6);
+                                    descs[i] = c.getString(7);
+                                    capacities[i] = c.getString(8);
+                                }
+                            }
+
+                            i++;
+                        }
+                        if (!(uc.getString(14).toString().equals(""))) {
+
+                            String[] day1 = uc.getString(15).split("@");
+                            String[] day2 = uc.getString(16).split("@");
+
+                            names[4] = uc.getString(14);
+                            days[4] = day1[0]+"/"+day2[0];
+                            hours[4] = day1[1]+"/"+day2[1];
+
+                            while(c.moveToNext()){
+                                if(c.getString(0).equals(names[4] )&& days[4].equals(c.getString(2)+"/"+c.getString(3)) && hours[4].equals(c.getString(4)+"/"+c.getString(5)) ){
+                                    codes[i] = c.getString(1);
+                                    inst[i] = c.getString(6);
+                                    descs[i] = c.getString(7);
+                                    capacities[i] = c.getString(8);
+                                }
+                            }
+
+                            i++;
+                        }
+
+
                     }
                 }
+
+
 
                 //I love arrays :)
                 tnames = new String[i];
@@ -163,6 +250,64 @@ public class FinalFragment extends Fragment {
                     @Override
                     public void onButtonClick(int pos) {
                         // DROP COURSE HERE @JAMES
+
+                        User u = new User(username, password);
+                        Cursor cursor = uDB.getData();
+
+                        String course1= "",course2= "",course3= "",course4= "",course5 = "";
+
+                        while(cursor.moveToNext()) {
+
+                            if (cursor.getString(0).equals(username)) {
+                                System.out.println("SUPERNATURAL TESTIUNG");
+                                if (!(cursor.getString(2).toString().equals(""))) {
+                                    System.out.println("HAS ONE AL:READY");
+                                    course1 = cursor.getString(2) + "_" + cursor.getString(3) + "_" + cursor.getString(4) + "_";
+
+                                    if (!(cursor.getString(5).toString().equals(""))) {
+
+                                        course2 = cursor.getString(5) + "_" + cursor.getString(6) + "_" + cursor.getString(7) + "_";
+
+                                        if (!(cursor.getString(8).toString().equals(""))) {
+
+                                            course3 = cursor.getString(8) + "_" + cursor.getString(9) + "_" + cursor.getString(10) + "_";
+
+                                            if (!(cursor.getString(11).toString().equals(""))) {
+
+                                                course4 = cursor.getString(11) + "_" + cursor.getString(12) + "_" + cursor.getString(13) + "_";
+
+                                                if (!(cursor.getString(14).toString().equals(""))) {
+
+                                                    course5 = cursor.getString(14) + "_" + cursor.getString(15) + "_" + cursor.getString(16) + "_";
+
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
+
+
+
+                        uDB.removeUser(u);
+
+                        if(!course1.equals("")){
+
+                            String[] temp = course1.split("_");
+                            System.out.println(course1);
+                            String[] day1 = temp[1].split("@");
+                            String[] day2 = temp[2].split("@");
+
+                            u.setCourse(temp[0], day1[0]+"/"+day2[0], day1[1]+"/"+day2[1]);
+
+                        }
+
+                        u.setCourse(tcodes[pos], tdays[pos], thours[pos]);
+
+                        uDB.addUser(u);
+
                     }
                     });
 
